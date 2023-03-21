560a3d2c4824d5ec47dc1e882cccbc61716a3a70
Daksh Kasliwal <91539425+stepper3117@users.noreply.github.com>
2023-03-21 09:42:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+x = 0;
+y = 0;
+
+speak_data= "";
+
+draw_apple = "";
+
+var SpeechRecognition = window.webkitSpeechRecognition;
+  
+var recognition = new SpeechRecognition();
+
+function start()
+{
+  document.getElementById("status").innerHTML = "System is listening please speak";  
+  recognition.start();
+} 
+ 
+recognition.onresult = function(event) {
+
+ console.log(event); 
+
+ content = event.results[0][0].transcript;
+
+    document.getElementById("status").innerHTML = "The speech has been recognized: " + content; 
+
+}
+
+function setup() {
+  canvas = createCanvas(900,600);0000
+}
+
+function draw() {
+  if(draw_apple == "set")
+  {
+    document.getElementById("status").innerHTML = to_number + " Apples drawn";
+    draw_apple = "";
+  }
+  for(var i=1; i<= to_number; i++)
+  {
+    x= Math.floor(Math.random()* 700);
+    y= Math.floor(Math.random()* 400);
+    image(apple, x, y, 50, 50);
+  }
+}
+
+function speak(){
+    var synth = window.speechSynthesis;
+
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utterThis);
+
+    speak_data = "";
+}
